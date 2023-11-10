64194e5cffc945acf17687a692d8f7dd864f2cdd
juanmarcossandoval <juanmarcos.sandoval@gmail.com>
2023-11-10 02:54:30 -0300

    first commit

---
+++ src/test/java/com/bibliotek/library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bibliotek.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
